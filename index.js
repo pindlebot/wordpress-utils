ecd4be3f912b947e1a3e5070d26af79269329157
Ben <menubar@protonmail.com>
2018-05-13 18:15:56 -0400

    new

---
+++ index.js
@@ -0,0 +1,18 @@
+const { promisify } = require('util.promisify')
+
+let wp
+let client
+
+module.exports = (wpClient) => {
+  if (wp && client === wpClient) {
+    return wp
+  }
+  client = wpClient
+  const keys = Object.keys(require('wordpress').Client.prototype)
+  keys.shift()
+  wp = keys.reduce((acc, key) => {
+    acc[key] = promisify(wpClient[key].bind(wpClient))
+    return acc
+  }, {})
+  return wp
+}
